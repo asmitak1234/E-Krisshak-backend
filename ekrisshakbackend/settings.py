f79082e02494b65cca9249ff4997af4a7e0def78
ASMITA KUMARI <166974264+asmitak1234@users.noreply.github.com>
2024-08-17 17:21:04 +0530

    hi

--- ekrisshakbackend/settings.py
+++ ekrisshakbackend/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = "django-insecure-p1(-_hpc#9%_r--v=4np482$^qgjvlj5zb+i24fyrvqbg!4p-@"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG')
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
@@ -78,18 +78,16 @@ WSGI_APPLICATION = 'ekrisshakbackend.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': config('DB_NAME'),
-        'HOST':config('DB_HOST'),
-        'PORT':'3306',
-        'USER':config('DB_USER'),
-        'PASSWORD':config('DB_PASSWORD'),
+        'NAME': 'ekrisshak',
+        'USER': 'root',
+        'PASSWORD': 'Asmita#132',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
